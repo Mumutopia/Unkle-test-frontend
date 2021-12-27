d8f076feb9b35439a11842b579b9ea836b0ef5e0
mumu <lemulot78@hotmail.com>
2021-12-27 20:21:44 +0100

    just added some flex  for the main wrapper

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Tooltip from "./Components/Tooltip";
 import Tooltip2 from "./Components/Tooltip2";
 
 function App() {
@@ -8,31 +7,35 @@ function App() {
       <h1 className="main-title"> Tooltip Prototype</h1>
 
       <section className="on-hover">
-        <Tooltip2 tooltipType="hover" position="left" content="hey there">
+        <Tooltip2 tooltipType="hover" position="left" content="Hello there">
           <div className="examples"> Left tip</div>
         </Tooltip2>
         <Tooltip2
           tooltipType="hover"
           position="top"
-          content="Hello there "
+          content="Hello there, this is a longer text "
         >
           <div className="examples"> Top tip</div>
         </Tooltip2>
-        <Tooltip2 tooltipType="hover" position="bottom" content="Hello there, this is  fdsfdsfds dsfdsfdsf sdf dsfds a longer sentence">
+        <Tooltip2 tooltipType="hover" position="bottom" content="Hello there">
           <div className="examples"> Bottom tip</div>
         </Tooltip2>
-        <Tooltip2 tooltipType="hover" position="right" content="heey there">
+        <Tooltip2 tooltipType="hover" position="right" content="heey there ! this is a longer sentence !">
           <div className="examples"> Right tip</div>
         </Tooltip2>
       </section>
       <section className="on-focus">
-        <Tooltip2 tooltipType="focus" position="left" content="heey there">
+        <Tooltip2 tooltipType="focus" position="left" content="heey there !">
           <input placeholder="Left input"></input>
         </Tooltip2>
         <Tooltip2 tooltipType="focus" position="bottom" content="heey there">
           <input placeholder="Bottom input"></input>
         </Tooltip2>
-        <Tooltip2 tooltipType="focus" position="top" content="heey there">
+        <Tooltip2
+          tooltipType="focus"
+          position="top"
+          content="heey thereeeeeeeeeeeeeeeeeee"
+        >
           <input placeholder="Top input"></input>
         </Tooltip2>
         <Tooltip2 tooltipType="focus" position="right" content="heey there">
